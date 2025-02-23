8c408ea1621e1921216472aa0cb9c89cfc204b36
jessicajcss <81390905+jessicajcss@users.noreply.github.com>
2025-02-23 15:19:35 -0300

    Update 00-access-newdata-thermo-git.R
    Select by column numbers instead of names

--- R/00-access-newdata-thermo-git.R
+++ R/00-access-newdata-thermo-git.R
@@ -51,7 +51,7 @@ thermo_repos2 <- httr::content(thermo_repos_raw)$tree |>
   purrr::map(unlist, recursive = TRUE)  |>
   purrr::map_dfr(tibble::enframe, .id = "id_repo") |>
   as.data.frame() |>
-  dplyr::select(id_repo, name, value) |>
+  dplyr::select(1, 2, 3) |>
   purrr::set_names(
     c("id_repo", "section", "value")
   )
