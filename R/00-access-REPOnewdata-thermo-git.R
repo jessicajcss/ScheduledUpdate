aa805baa8ab40f82cfa47ffbf82eaba3fc0d0417
jessicajcss <jessica.jcss@ufpr.br>
2025-02-25 01:04:14 -0300

    CONFIG r script to correct error following data access

--- R/00-access-REPOnewdata-thermo-git.R
+++ R/00-access-REPOnewdata-thermo-git.R
@@ -39,24 +39,96 @@ thermo_repos_raw <- purrr::map(quarto_orgs, function(repo) {
 # Print the result
 print(thermo_repos_raw)
 
-thermo_repos0 <- purrr::map(thermo_repos_raw, ~ .x$tree)
 
-print(thermo_repos0)
+thermo_repos <- thermo_repos_raw[[1]]$tree |>
+  #purrr::flatten() |>
+  purrr::map(unlist, recursive = TRUE)  |>
+  purrr::map_dfr(tibble::enframe, .id = "id_repo") |>
+  tidyr::pivot_wider() |>
+  dplyr::filter(stringr::str_detect(path,'.lsi')) |>
+  tidyr::separate(path, c('folder','filename'),'/')
 
-thermo_repos1 <- thermo_repos0 |>
-  purrr::map(unlist, recursive = TRUE) |>
-  purrr::map_dfr(function(x) {
-    tibble::enframe(x, name = "name", value = "value")
-  }, .id = "id_repo") |>
+# Check the resulting data frame
+
+load("./data_raw/file_path.Rda") # data previously downloaded
+
+url <- thermo_repos$url
+file_path_new <- url |>
   as.data.frame()
+# Updating only data since the last download ---
 
-print(thermo_repos1)
+result <- thermo_repos |>
+  dplyr::anti_join(file_path, by = "url")
 
-thermo_repos <- data.frame('id_repo' = thermo_repos1$id_repo,
-                           'name' = thermo_repos1$name,
-                           'value' = thermo_repos1$value)
 
-print(thermo_repos)
-class(thermo_repos)
+# paths to save unique url from files already downloaded to exclude them from the next loop
+file_path <- file_path_new
+
+#Looping and save file to local
+##We`ll loop through all the paths and request the data by generating url for each file, converting datatype of all columns to character and appending all files to a consolidated one.
+
+i <- 1
+out <- vector("list", nrow(result)) # vetor com o correspondente numero de variaveis meteorologicas
+
+for (i in seq(i, nrow(result))){
+  result$filename[i] <- sub(" ", "%20", result$filename[i])
+  path <- paste0("https://raw.githubusercontent.com/jessicajcss/Dados_GM_UFPR/refs/heads/main/", result$folder[i], "/", result$filename[i])
+  response <- httr::GET(path,
+                        httr::authenticate(Sys.getenv("GITHUB_PAT"), ""),
+                        Accept = "application/vnd.github.v3.raw")
+  file_content <- readLines(textConnection(httr::content(response, as = "text")))
+  out[[i]]$Cidade <- result$folder[i]
+  out[[i]]$value <- file_content[1]
+  print(path)
+  Sys.sleep(0.000001)
+}
+
+
+# Unificando o dataset
+
+output <- dplyr::bind_rows(out) |>
+  dplyr::distinct() # combine the output into a single data frame
+
+
+# Preparando o banco de dados
+data_git <- output |>
+  tidyr::separate(col = value,
+                  into = c("id", "date", "no2", "x4",
+                           "o3", "x6", "so2", "x8",
+                           "rh_sensor", "x10", "co", "x12",
+                           "pm2p5", "x14", "pm10", "x16", "x17"),
+                  sep = ",") |>
+  dplyr::mutate(date = as.POSIXct(date,
+                                  format = "%m/%d/%Y %I:%M:%S %p",
+                                  tz = 'America/Sao_Paulo')) |> #https://www.kaggle.com/discussions/questions-and-answers/382740
+  tidyr::drop_na() |>
+  dplyr::mutate(across(c(so2, no2, o3, co, pm2p5, pm10, rh_sensor), as.numeric)) |>
+  dplyr::select(Cidade, date, so2, no2, o3, co, pm2p5, pm10, rh_sensor) |>
+  dplyr::mutate(Cidade = dplyr::recode((Cidade),
+                                       "GM-RioBranco" = "Rio Branco do Sul", #02/08/23 15:50
+                                       "GM-AlmiranteTamandare" = "Almirante Tamandaré"))
+
+colnames(data_git) <- c('Cidade','date','SO2', 'NO2', 'O3', 'CO', 'PM2.5','PM10', 'rh_sensor')
+
+
+
+# Loading previou full dataset
+
+load("./data/data_thermo_update.Rda")
+
+data_thermo <- rbind(data_thermo, data_git) |>
+  unique() |>
+  dplyr::arrange(date)
+
+
+
+#################################################################
+########## UNIFICANDO UPDATED BANCO DE DADOS 'BASE' IN SITU & GIT
+
+
+# saving output dados até [22-02-2025 24h]
+save(data_thermo, file="./data/data_thermo_update.Rda")
+
+save(file_path, file="./data_raw/file_path.Rda")
 
 
